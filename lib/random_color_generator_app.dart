12b6d6723ccd7a81e45c51a3430680b280b3c130
nograve <nograveoff@gmail.com>
2021-06-24 16:48:44 +0300

    random_color_generator_app.dart added

---
+++ lib/random_color_generator_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RandomColorGeneratorApp extends StatelessWidget {
+  const RandomColorGeneratorApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Random Color Generator Application',
+      home: Container(),
+    );
+  }
+}
