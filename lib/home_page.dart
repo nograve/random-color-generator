e0926603d6a43408a005296a1bb1f13cb0a87598
nograve <nograveoff@gmail.com>
2021-06-25 17:33:58 +0300

    text font changed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -40,7 +40,7 @@ class _HomePageState extends State<HomePage> {
             child: Text(
               'Hey there',
               style: GoogleFonts.kirangHaerang(
-                fontSize: 60,
+                fontSize: 56,
               ),
             ),
           ),
