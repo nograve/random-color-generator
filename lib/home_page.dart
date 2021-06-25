9e513e9dbe52e2ace1998d84ecf9c7c77f396763
nograve <nograveoff@gmail.com>
2021-06-25 19:25:20 +0300

    added code saving to clipboard functionality

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -34,26 +34,68 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  void _onRGBCodeLongPress() {
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: _backgroundColor,
-      body: GestureDetector(
-        // define translucent behaviour to apply to all children in widget tree
-        behavior: HitTestBehavior.translucent,
-        onTap: _onScreenTap,
-        child: Container(
-          child: Center(
-            child: Text(
-              'Hey there',
-              style: GoogleFonts.kirangHaerang(
-                fontSize: 56,
-                color: _textColor,
+      // We need to use stack for reacting to tap on whole screen as inner layer
+        // and for saving code for clipboard functionality at the bottom right
+        // as the second outer layer.
+      body: Stack(
+        children: [
+          // GestureDetector to react to tap on whole screen
+          GestureDetector(
+            // Define translucent behaviour to apply to all children in widget tree
+            behavior: HitTestBehavior.translucent,
+            onTap: _onScreenTap,
+            child: Container(
+              child: Center(
+                child: Text(
+                  'Hey there',
+                  style: GoogleFonts.kirangHaerang(
+                    fontSize: 56,
+                    color: _textColor,
+                  ),
+                ),
               ),
             ),
           ),
-        ),
-      ),
+          // This aligns two string clicking by lower one you can
+          // save RGB code to the clipboard.
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Container(
+              margin: EdgeInsets.all(20),
+              child:  Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Hold code to copy',
+                      style: GoogleFonts.kirangHaerang(
+                        fontSize: 24,
+                        color: _textColor.withOpacity(0.5),
+                      ),
+                    ),
+                    GestureDetector(
+                      onLongPress: _onRGBCodeLongPress,
+                      child: Text(
+                        '#${_backgroundColor.red}.${_backgroundColor.green}.${_backgroundColor.blue}',
+                        style: GoogleFonts.kirangHaerang(
+                          fontSize: 40,
+                          fontStyle: FontStyle.italic,
+                          color: _textColor.withOpacity(0.75),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+        ],
+      )
     );
   }
 }
